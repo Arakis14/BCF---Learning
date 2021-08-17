0b532824ab6b75de2e53c94023a3ce9fd7747bf2
Arakis14 <wiktor.lasota@vp.pl>
2021-08-17 08:44:46 +0200

    print Matrix function in Main

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <iterator>
 #include <Matrix.h>
+
+void printMatrix(const std::vector<std::vector<int>>& x)
+{
+	for (int i = 0; i < x.size(); i++)
+	{
+		for (auto itr = x[i].begin(); itr != x[i].end(); itr++)
+		{
+			std::cout << *itr << " ";
+		}
+		std::cout << std::endl;
+	}
+};
 
 int main()
 {
-	std::cout << "Test" << std::endl;
 	std::vector<std::vector<int>> vec = {
 	{ 1, 2, 3 },
 	{ 4, 5, 6 },
 	{ 7, 8, 9 } };
+	printMatrix(vec);
 	Matrix *m1 = new Matrix(3, 3, vec);
 	delete m1;
 };
