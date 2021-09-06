b3be4e5f40eedb2b100f6d0182e4e939c4a69b31
Wiktor Lasota <wiktor.lasota@vp.pl>
2021-09-06 11:29:12 +0200

    Main.cpp cleanup and TravisCI

--- Main.cpp
+++ Main.cpp
@@ -20,47 +20,5 @@ void printMatrix(const std::vector<std::vector<int>>& x)
 int main()
 {
 	srand(time(NULL));
-	Matrix m1(5, 5); 
-	m1.fill();
-	m1.show();
-	//std::cout << m1.elementAt(2, 2) << std::endl;
-	//std::cout << m1.elementAt(10, 10);
-	Matrix m2(5, 5);
-	m2.fill();
-	m2.show();
-	Matrix m3(5, 5);
-	try {
-		m3 = m2 + m1;
-	}
-	catch (std::invalid_argument& e) {
-		std::cerr << e.what() << std::endl;
-	}
-	
-	m3.show();
-	try {
-		m3 += m2;
-	}
-	catch (std::invalid_argument& e) {
-		std::cerr << e.what() << std::endl;
-	}
-	m3.show();
 
-	try {
-		m3 = m2 - m1;
-	}
-	catch (std::invalid_argument& e) {
-		std::cerr << e.what() << std::endl;
-	}
-	m3.show();
-
-	std::cout << "Scalars:" << std::endl;
-
-	Matrix m4(3, 3);
-	m4.staticfill();
-	m4 += 5;
-	m4.show();
-	m4 = m4 - 3;
-	m4.show();
-	m4 = m4 * 2;
-	m4.show();
 };
